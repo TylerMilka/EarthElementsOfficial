3fcde0d439359f0fcdb3f9e4e0c9dedcd4cfe911
Tyler <tgmilka@gmail.com>
2017-02-01 19:31:33 -0600

    hmm

---
+++ app.js
@@ -0,0 +1,108 @@
+//Code Starts
+$(document).ready(function() {
+//Landscaping Javascript
+
+$('#landscapingServices').show();
+$('#weeklyServiceContent').slideUp();
+$('#lawnCareContent').slideUp();
+$('#treeServicesContent').slideUp();
+$('#installationsContent').slideUp();
+
+
+$("#landscapingService").click(function(){
+	$('#landscapingServicesContent').show();
+	$('#weeklyServiceContent').slideUp();
+	$('#lawnCareContent').slideUp();
+	$('#treeServicesContent').slideUp();
+	$('#installationsContent').slideUp();
+});
+
+$("#weeklyService").click(function(){
+	$('#weeklyServiceContent').show();
+	$('#lawnCareContent').slideUp();
+	$('#landscapingServicesContent').slideUp();
+	$('#treeServicesContent').slideUp();
+	$('#installationsContent').slideUp();
+});
+
+$("#lawnCare").click(function(){
+	$('#lawnCareContent').show();
+	$('#landscapingServicesContent').slideUp();
+	$('#weeklyServiceContent').slideUp();
+	$('#treeServicesContent').slideUp();
+	$('#installationsContent').slideUp();
+});
+
+$("#treeServices").click(function(){
+	$('#treeServicesContent').show();
+	$('#landscapingServicesContent').slideUp();
+	$('#weeklyServiceContent').slideUp();
+	$('#lawnCareContent').slideUp();
+	$('#installationsContent').slideUp();
+});
+
+
+$("#installations").click(function(){
+	$('#installationsContent').show();
+	$('#landscapingServicesContent').slideUp();
+	$('#weeklyServiceContent').slideUp();
+	$('#lawnCareContent').slideUp();
+	$('#treeServicesContent').slideUp();
+});
+
+
+//snow removal javascript
+$('#snowRemovalContent').show();
+$('#commercialServiceContent').slideUp();
+$('#residentialServiceContent').slideUp();
+$('#saltApplicationContent').slideUp();
+
+$("#snowRemoval").click(function(){
+	$('#snowRemovalContent').show();
+	$('#commercialServiceContent').slideUp();
+	$('#residentialServiceContent').slideUp();
+	$('#saltApplicationContent').slideUp();
+});
+
+$("#commercialService").click(function(){
+	$('#snowRemovalContent').slideUp();
+	$('#commercialServiceContent').show();
+	$('#residentialServiceContent').slideUp();
+	$('#saltApplicationContent').slideUp();
+});
+
+$("#residentialService").click(function(){
+	$('#snowRemovalContent').slideUp();
+	$('#commercialServiceContent').slideUp();
+	$('#residentialServiceContent').show();
+	$('#saltApplicationContent').slideUp();
+});
+
+$("#saltApplication").click(function(){
+	$('#snowRemovalContent').slideUp();
+	$('#commercialServiceContent').slideUp();
+	$('#residentialServiceContent').slideUp();
+	$('#saltApplicationContent').show();
+});
+
+})
+//     $("#hover").css("opacity", 0.5);
+//     $("#hover").hover(function() {
+//         $(this).animate({opacity: 1.0}, 500);
+//     }, function() {
+//         $(this).animate({opacity: 0.5}, 500);
+//     });
+
+// var rotation = 0;
+
+// jQuery.fn.rotate = function(degrees) {
+//     $(this).css({'transform' : 'rotate('+ degrees +'deg)'});
+//     return $(this);
+// };
+
+// $('#.logo').hover(function() {
+//     rotation += 90;
+//     $(this).rotate(rotation);
+
+// });
+
---
+++ assets/javascript/app.js
@@ -0,0 +1,32 @@
+ $(document).onload(function(){
+
+//put the different options into an array
+//look through the array
+//use this on the option that was clicked
+//display that option 
+
+
+console.log("fish");
+})
+
+
+// $("#landscpaingServics").click(function(){
+// 	console.log("fish");
+// })
+
+// //put all button option into variables
+// var landscapingServices = $("#landscapingServices");
+// var treeServices = $("#treeServices");
+// var installations = $("#installations");
+
+// //put variables into an array
+// var landscapingPageArray = [landscapingServices, treeServices, installations]
+
+// //function that runs through array based on click function
+// landscapingServicse.click(function(){
+// 	console.log(landscapingPageArray)
+// })
+// })
+
+
+// }
